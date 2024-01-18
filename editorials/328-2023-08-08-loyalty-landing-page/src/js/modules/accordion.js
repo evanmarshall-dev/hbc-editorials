e2f9806ef3481d646bdab1b3801d5c679e134daf
evanmarshall-dev <me@evanmarshall.dev>
2024-01-18 14:10:50 -0400

    🔧 chore(loyalty-landing-page): update accordion functionality and styles
    📝 fix(loyalty-landing-page): fix accordion button and panel accessibility

--- editorials/328-2023-08-08-loyalty-landing-page/src/js/modules/accordion.js
+++ editorials/328-2023-08-08-loyalty-landing-page/src/js/modules/accordion.js
@@ -5,10 +5,22 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("hbc-ed-accordion__button--active");
     const panel = this.nextElementSibling;
+    const expanded = this.getAttribute("aria-expanded") === "true";
+
+    this.setAttribute("aria-expanded", !expanded);
+    panel.setAttribute("aria-hidden", expanded);
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+
+  acc[i].addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      this.click();
     }
   });
 }
