d8948bb19c13320edb31ee3c00bccba4b5f4be2e
evanmarshall-dev <me@evanmarshall.dev>
2024-02-01 21:56:20 -0400

    🔧 chore(accordion): refactor accordion.js module 🔧 chore(modals): refactor modals.js module

--- editorials/2023/352-2024-01-15-store-locator-banner/src/js/modules/accordion.js
+++ editorials/2023/352-2024-01-15-store-locator-banner/src/js/modules/accordion.js
@@ -1,14 +1,57 @@
-// const acc = document.getElementsByClassName("hbc-ed-accordion__button");
+/**
+ * Adds click and keydown event listeners to all accordion buttons to toggle
+ * the active state and expand/collapse the corresponding panels.
+ */
+
+const acc = document.getElementsByClassName("hbc-ed-accordion__button");
 // let i;
 
-// for (i = 0; i < acc.length; i++) {
+acc.forEach((accItem) => {
+  accItem.addEventListener("click", (event) => {
+    event.stopPropagation();
+    this.classList.toggle("hbc-ed-accordion__button--active");
+    const panel = this.nextElementSibling;
+    const expanded = this.getAttribute("aria-expanded") === "true";
+
+    this.setAttribute("aria-expanded", !expanded);
+    panel.setAttribute("aria-hidden", expanded);
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      // panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
+    }
+  });
+
+  accItem.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      this.click();
+    }
+  });
+});
+
+// for (const i = 0; i < acc.length; i++) {
 //   acc[i].addEventListener("click", function () {
 //     this.classList.toggle("hbc-ed-accordion__button--active");
 //     const panel = this.nextElementSibling;
+//     const expanded = this.getAttribute("aria-expanded") === "true";
+
+//     this.setAttribute("aria-expanded", !expanded);
+//     panel.setAttribute("aria-hidden", expanded);
+
 //     if (panel.style.maxHeight) {
 //       panel.style.maxHeight = null;
 //     } else {
 //       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+
+//   acc[i].addEventListener("keydown", function (event) {
+//     if (event.key === "Enter" || event.key === " ") {
+//       event.preventDefault();
+//       this.click();
 //     }
 //   });
 // }
