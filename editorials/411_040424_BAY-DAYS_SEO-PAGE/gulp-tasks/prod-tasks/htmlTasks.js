111b061f105bb378fdfe8c23da0bd8f0664aef84
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-04-05 13:53:39 -0300

    EDITORIALS: Completed desktop and mobile content for the header section with CTAs. Also added in links and En/Fr copy. Pushed code to staging to test. Needed to decrease main wrapper x-padding for mobile.

--- editorials/411_040424_BAY-DAYS_SEO-PAGE/gulp-tasks/prod-tasks/htmlTasks.js
+++ editorials/411_040424_BAY-DAYS_SEO-PAGE/gulp-tasks/prod-tasks/htmlTasks.js
@@ -14,8 +14,8 @@ export default function htmlProd(srcPath, destPath) {
     .pipe(injectString.after("</title>", `\n<style>${cssContent}</style>`))
     .pipe(injectString.before("</body>", `<script>${jsContent}</script>\n`))
     .pipe(injectString.replace(/<!--[\s\S]*?-->/g, ""))
-    .pipe(injectString.replace(/<div class="cat-landing"|class="secondary-content"|class="home-slots">/g, '<div class="">'))
-    .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712236745383\/css\/global\.css"\s*\/>\s*<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/typography\.css"\s*\/>\s*<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/zellers-svg-font\.css"\s*\/>/gs, ''))
+    // .pipe(injectString.replace(/<div class="cat-landing"|class="secondary-content"|class="home-slots">/g, '<div class="">'))
+    // .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712236745383\/css\/global\.css"\s*\/>\s*<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/typography\.css"\s*\/>\s*<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/zellers-svg-font\.css"\s*\/>/gs, ''))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest(destPath));
   // .on("end", resolve)
