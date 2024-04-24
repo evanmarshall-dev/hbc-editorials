b86afed213fc6f3c67c7c54c44ec4af55867b530
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-04-24 16:30:53 -0300

    EDITORIALS: Created a directory for all 2023 editorial work and moved all week 300 content into it.

---
+++ editorials/2023/328-2023-08-08-loyalty-landing-page/src/js/modules/accordion.js
@@ -0,0 +1,26 @@
+const acc = document.getElementsByClassName('hbc-ed-accordion__button');
+let i;
+
+for (i = 0; i < acc.length; i += 1) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('hbc-ed-accordion__button--active');
+    const panel = this.nextElementSibling;
+    const expanded = this.getAttribute('aria-expanded') === 'true';
+
+    this.setAttribute('aria-expanded', !expanded);
+    panel.setAttribute('aria-hidden', expanded);
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
+    }
+  });
+
+  acc[i].addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.click();
+    }
+  });
+}
