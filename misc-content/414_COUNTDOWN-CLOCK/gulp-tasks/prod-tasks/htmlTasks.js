a799b8616e4b99c571b2232cca147dd7b44cb083
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-05-02 16:19:58 -0300

    EDITORIALS - MISC CONTENT: Finished up countdown clock content fix. Pushed to stage and prod. TODO: Figure out a better way to switch JS added content from ENG or FR depending on labaie.com or thebay.com instead of having it inline for FR.

--- misc-content/414_COUNTDOWN-CLOCK/gulp-tasks/prod-tasks/htmlTasks.js
+++ misc-content/414_COUNTDOWN-CLOCK/gulp-tasks/prod-tasks/htmlTasks.js
@@ -14,10 +14,11 @@ export default function htmlProd(srcPath, destPath) {
     .pipe(injectString.after("</title>", `\n<style>${cssContent}</style>`))
     .pipe(injectString.before("</body>", `<script>${jsContent}</script>\n`))
     .pipe(injectString.replace(/<!--[\s\S]*?-->/g, ""))
-    .pipe(injectString.replace(/class="(cat-landing|secondary-content|home-slots)"/g, 'class=""'))
+    .pipe(injectString.replace(/class="(home-slots)"/g, 'class=""'))
     .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712236745383\/css\/global\.css"\s*\/>/gs, ''))
     .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/typography\.css"\s*\/>/gs, ''))
     .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1712130735115\/css\/zellers-svg-font\.css"\s*\/>/gs, ''))
+    .pipe(injectString.replace(/<script\s+src="https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/jquery\/3\.6\.0\/jquery\.min\.js"\s+integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP\+u1T9qYdvdihz0PPSiiqn\/\+\/3e7Jo4EaG7TubfWGUrMQ=="\s+crossorigin="anonymous"\s+referrerpolicy="no-referrer"\s*><\/script>/gs, ''))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest(destPath));
   // .on("end", resolve)
