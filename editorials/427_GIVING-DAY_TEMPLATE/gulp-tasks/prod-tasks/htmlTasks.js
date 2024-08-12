d7354b1eb681d028e64c96d27c992d34690cdd5b
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-08-12 17:19:22 -0300

    HBC EDITORIALS: GIVING DAY page completed for English. Requires a once over to refine any styles and refactor any code.

--- editorials/427_GIVING-DAY_TEMPLATE/gulp-tasks/prod-tasks/htmlTasks.js
+++ editorials/427_GIVING-DAY_TEMPLATE/gulp-tasks/prod-tasks/htmlTasks.js
@@ -1,23 +1,43 @@
-import pkg from "gulp";
-import injectString from "gulp-inject-string";
-import fs from "fs";
-import htmlmin from "gulp-htmlmin";
+import pkg from 'gulp';
+import injectString from 'gulp-inject-string';
+import fs from 'fs';
+import htmlmin from 'gulp-htmlmin';
 
 const { src, dest } = pkg;
 
 export default function htmlProd(srcPath, destPath) {
-  const cssContent = fs.readFileSync("prod/css/styles.css", "utf8");
-  const jsContent = fs.readFileSync("prod/js/app.js", "utf8");
+  const cssContent = fs.readFileSync('prod/css/styles.css', 'utf8');
+  const jsContent = fs.readFileSync('prod/js/app.js', 'utf8');
 
   // return new Promise((resolve, reject) => {
   src(srcPath)
-    .pipe(injectString.after("</title>", `\n<style>${cssContent}</style>`))
-    .pipe(injectString.before("</body>", `<script>${jsContent}</script>\n`))
-    .pipe(injectString.replace(/<!--[\s\S]*?-->/g, ""))
-    .pipe(injectString.replace(/class="(cat-landing|secondary-content|home-slots)"/g, 'class=""'))
-    .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1721787329464\/css\/global\.css"\s*\/>/gs, ''))
-    .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1721787329464\/css\/typography\.css"\s*\/>/gs, ''))
-    .pipe(injectString.replace(/<link\s+rel="stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1721787329464\/css\/zellers-svg-font\.css"\s*\/>/gs, ''))
+    .pipe(injectString.after('</title>', `\n<style>${cssContent}</style>`))
+    .pipe(injectString.before('</body>', `<script>${jsContent}</script>\n`))
+    .pipe(injectString.replace(/<!--[\s\S]*?-->/g, ''))
+    .pipe(
+      injectString.replace(
+        /class="(cat-landing|secondary-content|home-slots)"/g,
+        'class=""',
+      ),
+    )
+    .pipe(
+      injectString.replace(
+        /<link\s+rel="preconnecct stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1723170782328\/css\/global\.css"\s*\/>/gs,
+        '',
+      ),
+    )
+    .pipe(
+      injectString.replace(
+        /<link\s+rel="preconnect stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1723170782328\/css\/typography\.css"\s*\/>/gs,
+        '',
+      ),
+    )
+    .pipe(
+      injectString.replace(
+        /<link\s+rel="preconnect stylesheet"\s+href="https:\/\/www\.thebay\.com\/on\/demandware\.static\/Sites-TheBay-Site\/-\/en_CA\/v1723170782328\/css\/zellers-svg-font\.css"\s*\/>/gs,
+        '',
+      ),
+    )
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest(destPath));
   // .on("end", resolve)
