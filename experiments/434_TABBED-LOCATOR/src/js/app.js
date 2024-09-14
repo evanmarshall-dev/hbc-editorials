6cda382e9a8da109429c0225f554913622f2b187
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-09-13 22:51:58 -0300

    HBC EDITORIALS: TABBED LOCATOR EXPERIMENT, minor revision to js glob in gulpfile.js. Updated stores.json to stores.js and revised the object for js to be assigned to a variable. Added code to app.js to parse the stores.js array objects and innerHTML to insert into the DOM.

--- experiments/434_TABBED-LOCATOR/src/js/app.js
+++ experiments/434_TABBED-LOCATOR/src/js/app.js
@@ -1,1 +1,60 @@
-// console.log("THIS IS A TEST APP.JS FILE");
+/* eslint-disable no-undef */
+
+// console.log(data.bc[2].storeName);
+
+const display = document.querySelector('#display-data');
+
+const getData = () => data.bc;
+
+// console.log(getData());
+
+const displayStores = () => {
+  // Store data in payload.
+  const payload = getData();
+  console.log(payload);
+
+  // Ensure payload is an array before mapping
+  if (!Array.isArray(payload)) {
+    console.error('Payload is not an array:', payload);
+    return;
+  }
+
+  // Store mapped data in dataDisplay.
+  // object stores each object as we iterate the array.
+  const dataDisplay = payload
+    .map((object) => {
+      console.log(object);
+      // Deconstruct properties out of our object.
+      const {
+        storeName,
+        streetAddress,
+        city,
+        province,
+        postalCode,
+        zellersFloor,
+        phoneNumber,
+        hoursLink,
+        // mapLink,
+        directionsLink,
+      } = object;
+
+      // Return HTML from object.
+      return `
+      <div>
+        <p>Store Name: ${storeName}</p>
+        <p>Street Address: ${streetAddress}</p>
+        <p>City: ${city}</p>
+        <p>Province: ${province}</p>
+        <p>Postal Code: ${postalCode}</p>
+        <p>Zellers Floor: ${zellersFloor}</p>
+        <p>Phone Number: ${phoneNumber}</p>
+        <p>Hours Link: <a href="${hoursLink}" target="_blank">View Hours</a></p>
+        <p>Directions Link: <a href="${directionsLink}" target="_blank">Get Directions</a></p>
+      </div>
+    `;
+    })
+    .join('');
+  // Insert data into DOM we call the display variable above.
+  display.innerHTML = dataDisplay;
+};
+displayStores();
