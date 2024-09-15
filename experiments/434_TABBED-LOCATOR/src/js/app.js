b9a4033373ae74d93fc9d3f64c11df6db69a4ced
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-09-14 22:30:47 -0300

    HBC EDITORIALS: TABBED LOCATOR EXPERIMENT, Updated the app.js code to allow for array filtering for some keywords so that I can add several keywords to be shown based on store name. Also added conditional in the innerHTML based on data.js key of hasZellers to be true or false boolean to display SVG and Zellers floor or not.

--- experiments/434_TABBED-LOCATOR/src/js/app.js
+++ experiments/434_TABBED-LOCATOR/src/js/app.js
@@ -4,28 +4,20 @@ const display = document.querySelector('.ed-container--tab');
 
 const getData = (region) => data[region];
 
-const displayStores = (region, keyword) => {
+const displayStores = (region, ...keywords) => {
   // Store data in payload.
   const payload = getData(region);
-  // ? console.log(payload);
 
-  // Ensure payload is an array before mapping
-  // ? if (!Array.isArray(payload)) {
-  //   ? console.error('Payload is not an array:', payload);
-  //   ? return;
-  // ? }
-
-  // Filter the payload based on the keyword in storeName.
+  // Filter the payload based on the keywords in storeName.
   const filteredPayload = payload.filter((object) =>
-    object.storeName.toLowerCase().includes(keyword.toLowerCase()),
+    keywords.some((keyword) =>
+      object.storeName.toLowerCase().includes(keyword.toLowerCase()),
+    ),
   );
 
   // Store mapped data in dataDisplay.
-  // object stores each object as we iterate the array.
   const dataDisplay = filteredPayload
     .map((object) => {
-      // ? console.log(object);
-      // Deconstruct properties out of our object.
       const {
         storeName,
         streetAddress,
@@ -37,6 +29,7 @@ const displayStores = (region, keyword) => {
         hoursLink,
         mapLink,
         directionsLink,
+        hasZellers,
       } = object;
 
       // Return HTML from object.
@@ -52,41 +45,45 @@ const displayStores = (region, keyword) => {
               ${streetAddress}<br />
               ${city}, ${province} ${postalCode}
             </p>
-            <div class="ed-container--store__zellers ed-util-py--1pnt5">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-labelledby="title desc"
-              >
-                <title id="title">Zellers Circular Logo</title>
-                <desc id="desc">
-                  A red circle with a white Zellers letter Z in the
-                  center.
-                </desc>
-                <g clip-path="url(#clip0_634_5624)">
-                  <rect width="16" height="16" rx="8" fill="#D32027" />
-                  <path
-                    d="M15.0057 7.6657V7.66705L1.70535 15.9566L1.63057 16L0.995117 14.0634L9.14905 4.23454L10.1821 2.98939L10.196 2.97036L10.1693 2.98424L3.09302 6.25374L2.49452 4.42139L12.4758 0L13.078 1.83481V1.83616L13.0668 1.85261L13.0119 1.92762L5.7985 11.4578L5.74375 11.5262L5.83018 11.4778L14.4446 6.10138L15.0057 7.6657Z"
-                    fill="white"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_634_5624">
-                    <rect width="16" height="16" rx="8" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-              <h4 class="ed-type--h4 ed-util-pl--pnt5">
-                Zellers
-                <span class="ed-util--normal ed-util--sans"
-                  >${zellersFloor}</span
-                >
-              </h4>
-            </div>
+            ${
+              hasZellers
+                ? `<div class="ed-container--store__zellers ed-util-py--1pnt5">
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-labelledby="title desc"
+                  >
+                    <title id="title">Zellers Circular Logo</title>
+                    <desc id="desc">
+                      A red circle with a white Zellers letter Z in the
+                      center.
+                    </desc>
+                    <g clip-path="url(#clip0_634_5624)">
+                      <rect width="16" height="16" rx="8" fill="#D32027" />
+                      <path
+                        d="M15.0057 7.6657V7.66705L1.70535 15.9566L1.63057 16L0.995117 14.0634L9.14905 4.23454L10.1821 2.98939L10.196 2.97036L10.1693 2.98424L3.09302 6.25374L2.49452 4.42139L12.4758 0L13.078 1.83481V1.83616L13.0668 1.85261L13.0119 1.92762L5.7985 11.4578L5.74375 11.5262L5.83018 11.4778L14.4446 6.10138L15.0057 7.6657Z"
+                        fill="white"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_634_5624">
+                        <rect width="16" height="16" rx="8" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  <h4 class="ed-type--h4 ed-util-pl--pnt5">
+                    Zellers
+                    <span class="ed-util--normal ed-util--sans"
+                      >${zellersFloor}</span
+                    >
+                  </h4>
+                </div>`
+                : ''
+            }
             <a class="ed-type--link" href="tel:+1${phoneNumber}"
               >${phoneNumber}</a
             >
@@ -132,7 +129,7 @@ const displayStores = (region, keyword) => {
   // Insert data into DOM we call the display variable above.
   display.innerHTML = dataDisplay;
 };
+
 // Example calls
 displayStores('bc', 'Vancouver'); // Display BC stores with 'Vancouver' in the store name
-// ? displayStores('ab', 'Calgary'); // Display AB stores with 'Calgary' in the store name
-// ? displayStores('qc', 'Montreal'); // Display QC stores with 'Montreal' in the store name
+// displayStores('on', 'Toronto', 'Eglinton'); // Display ON stores with 'Toronto' and 'Eglinton' in the store name
