224886e6ba3914e217dc017cb042bc519b52d44a
evanmarshall-saks <evan.marshall@sakscloudservices.com>
2024-09-30 17:07:20 -0300

    HBC EDITORIALS: TABBED GLUCKSTEIN DESIGN TIPS PAGE, added new directory for this project. Used store locator scripts and modified them a bit to target data-content attributes for each button related to classes for each module type in SFCC which I want to display per that button. Also set a default state to look like the first button click.

---
+++ editorials/438_GLUCKSTEIN-DESIGN-TIPS/src/js/modules/tabs.js
@@ -0,0 +1,66 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const tabLinks = document.querySelectorAll('.ed-button--tab');
+//   const tabContents = document.querySelectorAll('.TEST_VISUAL-NAV_GLUCKSTEIN');
+
+//   tabLinks.forEach((link) => {
+//     link.addEventListener('click', function (event) {
+//       event.preventDefault();
+
+//       const tabId = this.getAttribute('href').substring(1);
+
+//       // Hide all tab contents
+//       tabContents.forEach((content) => {
+//         content.setAttribute('hidden', 'true');
+//       });
+
+//       // Reset aria-selected attribute for all tabs
+//       tabLinks.forEach((tabLink) => {
+//         tabLink.setAttribute('aria-selected', 'false');
+//       });
+
+//       // Show the selected tab content
+//       document.getElementById(tabId).removeAttribute('hidden');
+//       this.setAttribute('aria-selected', 'true');
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tabLinks = document.querySelectorAll('.ed-button--tab');
+
+  tabLinks.forEach((link) => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const contentClasses = this.getAttribute('data-contents').split(' ');
+
+      // Hide all tab contents
+      document
+        .querySelectorAll(
+          '.TEST_VISUAL-NAV_GLUCKSTEIN, .multi-image-container, .shop-beauty-banner, .new-prodict-slot, .evergreen-promo',
+        )
+        .forEach((content) => {
+          content.setAttribute('hidden', 'true');
+        });
+
+      // Reset aria-selected attribute for all tabs
+      tabLinks.forEach((tabLink) => {
+        tabLink.setAttribute('aria-selected', 'false');
+      });
+
+      // Show the selected tab contents
+      contentClasses.forEach((className) => {
+        document.querySelectorAll(`.${className}`).forEach((content) => {
+          content.removeAttribute('hidden');
+        });
+      });
+
+      this.setAttribute('aria-selected', 'true');
+    });
+  });
+
+  // Simulate click on the first button to load default tab content
+  if (tabLinks.length > 0) {
+    tabLinks[0].click();
+  }
+});
